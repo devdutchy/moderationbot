a9ae4108e286fc57ce2e3f78d1c37c01eafa1b0e
ModzOn <modzongaming@gmail.com>
2017-11-10 15:37:22 +0100

    im getting closer

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -10,7 +10,7 @@ module.exports = class GuildSetupCommand extends Command {
       description: "Sets the guild up for moderation."
     });
   }
-  async run(msg, args) {
+  async run(msg, args) { // eslint-disable-line complexity
     // Permission check
     if (msg.author.id !== msg.guild.ownerID || this.client.isOwner(msg.author.id)) {
       return msg.reply("you do not have permission to run this command.");
@@ -29,71 +29,59 @@ module.exports = class GuildSetupCommand extends Command {
       if (!key.length) return msg.reply("you did not specify a role.");
       if (!this.validate(key[0], "role", msg)) return msg.reply("you provided an invalid role.");
       const modroleExists = await GuildModel.findOne({ where: { guild: msg.guild.id } });
-      if (msg.mentions.roles && modroleExists) {
-        const updatedRows = await GuildModel.update({ modrole: msg.mentions.roles.first().id }, { where: { guild: msg.guild.id } });
+      if (modroleExists) {
+        let role = msg.mentions.roles.first().id || key[0];
+        const updatedRows = await GuildModel.update({ modrole: role }, { where: { guild: msg.guild.id } });
         if (updatedRows > 0) {
           return msg.reply("successfully updated the moderator role.");
         }
         return msg.reply("I couldn't update the modrole.");
-      } else if (msg.mentions.roles && !modroleExists) {
-        const updatedRows = await GuildModel.create({
-          guild: msg.guild.id,
-          modrole: msg.mentions.roles.first().id,
-          muterole: null,
-          totalCases: 0,
-          modlogChannel: null,
-          joinlogChannel: null,
-          joinMessage: null,
-          leaveMessage: null,
-          messageChannel: null,
-          isEnabledMuteRole: false,
-          isEnabledModlog: false,
-          isEnabledJoinlog: false,
-          isEnabledJoinmessage: false,
-          isEnabledJoinrole: false,
-          isEnabledLeavemessage: false
-        });
-        if (updatedRows > 0) {
-          return msg.reply("successfully enabled the moderator role.");
-        }
-        return msg.reply("I couldn't enable the modrole.");
       } else if (!modroleExists) {
-        let exists = msg.guild.roles.has(key[0]);
-        if (!exists) return msg.reply("couldn't find a role with that ID.");
-        let role = msg.guild.roles.get(key[0]);
-        const updatedRows = await GuildModel.create({
-          guild: msg.guild.id,
-          modrole: role.id,
-          muterole: null,
-          totalCases: 0,
-          modlogChannel: null,
-          joinlogChannel: null,
-          joinMessage: null,
-          leaveMessage: null,
-          messageChannel: null,
-          isEnabledMuteRole: false,
-          isEnabledModlog: false,
-          isEnabledJoinlog: false,
-          isEnabledJoinmessage: false,
-          isEnabledJoinrole: false,
-          isEnabledLeavemessage: false
-        });
-        if (updatedRows > 0) {
+        try {
+          await GuildModel.create({
+            guild: msg.guild.id,
+            modrole: msg.mentions.roles.first().id,
+            muterole: null,
+            totalCases: 0,
+            modlogChannel: null,
+            joinlogChannel: null,
+            joinMessage: null,
+            leaveMessage: null,
+            messageChannel: null,
+            isEnabledMuteRole: false,
+            isEnabledModlog: false,
+            isEnabledJoinlog: false,
+            isEnabledJoinmessage: false,
+            isEnabledJoinrole: false,
+            isEnabledLeavemessage: false
+          });
           return msg.reply("successfully enabled the moderator role.");
+        } catch (err) {
+          return msg.reply("I couldn't enable the modrole.");
         }
-        return msg.reply("I couldn't enable the modrole.");
       }
     }
     if (option === "modlog") {
       if (!this.validate(key[0], "channel", msg)) return msg.reply("you provided an invalid channel.");
+      let channel = msg.mentions.channels.first().id || key[0];
+      const exists = await GuildModel.findOne({ where: { guild: msg.guild.id } });
+      if (exists) {
+        const affectedRows = await GuildModel.update({
+          modlogChannel: channel,
+          isEnabledModlog: true
+        }, { where: { guild: msg.guild.id } });
+        if (affectedRows > 1) {
+          return msg.reply("I successfully updated the mod-log channel.");
+        }
+        return msg.reply("I couldn't update the modlog channel.");
+      }
     }
     if (option === "mute") {
-      
+      if (!this.validate(key[0], "role", msg)) return msg.reply("you provided an invalid role.");
     }
     if (option === "joinrole") {
-      
+      if (!this.validate(key[0], "role", msg)) return msg.reply("you provided an invalid role.")
     }
-    
     return null;
   }
   async validate(value, type, msg) {
